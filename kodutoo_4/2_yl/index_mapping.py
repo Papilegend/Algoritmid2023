cf117aaae30ecb5da958dffc49bd51160995b7a2
Tristan <119224484+Papilegend@users.noreply.github.com>
2023-12-01 15:41:03 +0200

    Add files via upload

---
+++ kodutoo_4/2_yl/index_mapping.py
@@ -0,0 +1,8 @@
+# index map listi kujuna, kus indeks + 1 näitab, kes mitmendal postitsioonil järjekorras asub
+
+prioriteedi_jarjekord = ["Kalle", "Mait" "Jaanus", "Hans", "Peeter", "Karl", "Guido", "Peep", "Eigo"]
+
+nimi = "Guido" 
+
+print(prioriteedi_jarjekord.index("Guido") + 1)
+
