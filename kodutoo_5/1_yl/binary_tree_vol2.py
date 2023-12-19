910ae8c1c1d74ce4fddcbc1f3f4202b3d5cfc08d
Tristan <119224484+Papilegend@users.noreply.github.com>
2023-12-19 15:52:41 +0200

    Add files via upload

---
+++ kodutoo_5/1_yl/binary_tree_vol2.py
@@ -0,0 +1,66 @@
+""" Tegmist on objekt orienteeritud koodiga, mis teeb teatud ettevõtte ametite tähtsuse järgi
+ettevõte ametipuu, mis on binary tree. Väga efektiivne rakendus see pole, kuid näitab ära, kuidas töötab binary tree"""
+
+
+
+class tipp:
+    
+    def __init__(self, nimi, vanus):
+        self.vasak = None
+        self.parem = None
+        self.nimi = nimi  
+        self.vanus = vanus # Kasutan binary treesse lisamiseks ametitel "ametivanust"
+
+    def sisesta(self, nimi, vanus):
+
+    # Kui binary trees pole ühtegi elementi, siis lisa vaadeldav element puu tüveks
+        if self.vanus is None:
+            self.vanus = vanus
+            self.nimi = nimi
+
+    # Kui binary trees on tüveelement olemas, siis võrdle, kas see on suurem või väiksem tüveelemendist
+        else:
+            if vanus < self.vanus:
+
+    # Kui vasakpoolset elementi pole, siis lisa see vasakule
+                if self.vasak is None:
+                    self.vasak = tipp(nimi, vanus)
+
+    # Kui vasakpoolne element on olemas, siis korda protsessi võttes olemasolev vasak element vaadeldavaks tüveks
+                else:
+                    self.vasak.sisesta(nimi, vanus)
+            elif vanus > self.vanus:
+
+    # Kui vasakpoolset elementi pole, siis lisa element paremale
+                
+                if self.parem is None:
+                    self.parem = tipp(nimi, vanus)
+
+     # Kui vasakpoolne element on olemas, siis korda protsessi võttes olemasolev vasak element vaadeldavaks tüveks
+                else:
+                    self.parem.sisesta(nimi, vanus)
+
+def printimine(tuvi):
+    if tuvi is None:
+        return
+    else:
+        print(f"Ametinimetus: {tuvi.nimi}")  # Modify print statement to display name and age
+        printimine(tuvi.vasak)
+        printimine(tuvi.parem)
+
+if __name__ == "__main__":
+
+    # Teen binary tree tüveks tegevjuhi ameti
+    tuvi = tipp("Tegevjuht", 30)  
+
+    # Lisan tegejuhile ametivanuste abil juurde teised ametid
+    tuvi.sisesta("Produktsioonijuht", 25)  
+    tuvi.sisesta("Turundusjuht", 35)
+    tuvi.sisesta("Tööde juhataja", 22)
+    tuvi.sisesta("Müügijuht", 40)
+    tuvi.sisesta("Tehasetööline", 24)
+    tuvi.sisesta("Tehasetööline", 21)
+    tuvi.sisesta("Müügitöötaja", 41)
+    tuvi.sisesta("Müügitöötaja", 39)
+
+printimine(tuvi)  
