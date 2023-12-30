cf965be51829f8b9ddf47c4daecc29fd1dde4117
Tristan <119224484+Papilegend@users.noreply.github.com>
2023-12-30 15:37:15 +0200

    Add files via upload

---
+++ Projekt/andmed.py
@@ -0,0 +1,12 @@
+graaf = {
+    "Tallinn":  {"Helsingi" : {"kaugus": 90,   "hind": 100},  "Pariis"  : {"kaugus": 1800,  "hind": 130},  "London"   : {"kaugus": 1820,  "hind": 120},  "Istanbul" : {"kaugus": 2000,  "hind": 300},  "Madrid": {"kaugus": 2800, "hind": 220}},
+    "Helsingi": {"Tallinn"  : {"kaugus": 90,   "hind": 100},  "London"  : {"kaugus": 1820,  "hind": 160},  "New York" : {"kaugus": 6500,  "hind": 2500}, "Kairo"    : {"kaugus": 7500,  "hind": 400}},
+    "New York": {"London"   : {"kaugus": 5500, "hind": 2200}, "Helsingi": {"kaugus": 6500,  "hind": 2500}, "Tokyo"    : {"kaugus": 11000, "hind": 800},  "Sydney"   : {"kaugus": 15000, "hind": 1600}},
+    "London":   {"Madrid"   : {"kaugus": 1200, "hind": 65},   "Tallinn" : {"kaugus": 1800,  "hind": 120},  "Helsinki" : {"kaugus": 1820,  "hind": 160},  "New York" : {"kaugus": 5500,  "hind": 2200}},
+    "Madrid":   {"Pariis"   : {"kaugus": 1000, "hind": 70},   "London"  : {"kaugus": 1200,  "hind": 65},   "Tallinn"  : {"kaugus": 2800,  "hind": 220}},
+    "Pariis":   {"Madrid"   : {"kaugus": 1000, "hind": 70},   "Tallinn" : {"kaugus": 1800,  "hind": 130},  "Kairo"    : {"kaugus": 3200,  "hind": 400},  "Sydney"   : {"kaugus": 17000, "hind": 900}},
+    "Cairo":    {"Istanbul" : {"kaugus": 1200, "hind": 250},  "Pariis"  : {"kaugus": 7500,  "hind": 400},  "Helsinki" : {"kaugus": 7500,  "hind": 400}},
+    "Istanbul": {"Cairo"    : {"kaugus": 1200, "hind": 250},  "Tallinn" : {"kaugus": 2000,  "hind": 300},  "Tokyo"    : {"kaugus": 9000,  "hind": 800},  "Sydney"   : {"kaugus": 15000, "hind": 700}},
+    "Tokyo":    {"Sydney"   : {"kaugus": 8000, "hind": 1000}, "Istanbul": {"kaugus": 9000,  "hind": 900},  "New York" : {"kaugus": 11000, "hind": 800}},
+    "Sydney":   {"Tokyo"    : {"kaugus": 8000, "hind": 1000}, "Istanbul": {"kaugus": 15000, "hind": 700},  "Pariis"   : {"kaugus": 17000, "hind": 900},  "New York" : {"kaugus": 18000, "hind": 1600}}
+}
